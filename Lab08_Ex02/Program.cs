d6271bccced8f6a36ca97170f9742c9460714599
SunisaManureem <sunisa2561a@gmail.com>
2025-02-03 20:49:04 +0700

    ส่งงาน

---
+++ Lab08_Ex02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Person p = new Person();
+        p.Name = "Rambo";  // ใช้ property
+        p.Id = "1987";     // ใช้ property
+        p.Income = 2500;   // ใช้ property
+
+        // แสดงประเภทของ p
+        System.Console.WriteLine($"Type of p is {p.GetType()}");
+
+        // แสดงประเภทของสมาชิก
+        System.Console.WriteLine($"p.Name = {p.Name}\ttype = {p.Name.GetType()}");
+        System.Console.WriteLine($"p.Id = {p.Id}\ttype = {p.Id.GetType()}");
+        System.Console.WriteLine($"p.Income = {p.Income}\ttype = {p.Income.GetType()}");
+    }
+}
+
+public class Person
+{
+    // ใช้ public properties เพื่อให้เข้าถึงได้จากภายนอก
+    public string Name { get; set; }
+    public string Id { get; set; }
+    public int Income { get; set; }
+}
